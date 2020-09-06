b2503f4b85ee3d70f06ca5215f2c2269525bc5e8
dmitri9149 <dmitri.as.name@gmail.com>
2020-09-06 15:22:41 +0300

    some random answers generator

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use rand::Rng;
 
 
 fn main() {
@@ -17,7 +18,24 @@ fn main() {
 
     println!("I see you say {} to me!", request);
     println!("It is very vise from your side,\nI will think about it a little bit \
-            and will reply to you soon.") 
+            and will reply to you soon.\n");
+
+    let random_number = rand::thread_rng().gen_range(1,6);
+
+    if random_number == 1 {
+        println!("By the way, what is your favourite greeting ? Hey ? Hello ?");
+    } else if random_number == 2 {
+        println!("....hmmm.... to think about it takes a longer time than I expected...");
+    } else if random_number == 3 {
+        println!(".....just a moment....please...");
+    } else if random_number == 4 {
+        println!("By the way, do you think the 'Rosa' is a good name for a bot ?")     
+    } else {
+        println!("OK ! I almoust ready. But if you have something to finilise...like \
+                your university course in Quantum Mechanics, may be it is a good idea \
+                for you to do it now, you still have some time.\n");
+    }      
+
 
 
 }
